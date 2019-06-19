55d6cfcdd0fc3ef0fbce0da6b4c0d69760d8ffd3
Quoc Vuong <hoangquocvuong92@gmail.com>
2019-06-19 11:15:21 +0700

    Fix conflict

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	find . \! -user redis -exec chown redis '{}' +
-	exec gosu redis "$0" "$@"
+	exec su-exec redis "$0" "$@"
 fi
 
 exec "$@"
